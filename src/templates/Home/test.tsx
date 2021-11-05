45962b156368bce2e8d42937a7975b26e0d5f7dc
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-11-05 10:32:13 -0300

    feat: styled created to home page

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,11 +1,28 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
 import Home from '.'
+
+const props = {
+  banners: [bannersMock[0]],
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
@@ -14,7 +31,7 @@ describe('<Home />', () => {
   })
 
   it('should render the sections', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(

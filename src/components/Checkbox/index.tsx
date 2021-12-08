6533427867ed8ffdcb99ff17a91d88eac503158f
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-12-08 20:48:29 -0300

    feat: new prop added  to start component on checked and function to check component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,24 +1,51 @@
+import { useState } from 'react'
+import { InputHTMLAttributes } from 'react'
 import * as S from './styles'
 
 export type CheckboxProps = {
+  isChecked?: boolean
+  onCheck?: (status: boolean) => void
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+  value?: string | ReadonlyArray<string> | number
+} & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
+  onCheck,
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
-    {!!label && (
-      <S.Label labelColor={labelColor} htmlFor={labelFor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  isChecked = false,
+  value,
+  ...props
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(isChecked)
+
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    !!onCheck && onCheck(status)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+        value={value}
+        {...props}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Checkbox
--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -8,7 +8,27 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  argTypes: {
+    onCheck: { action: 'checked' }
   }
 } as Meta
 
-export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
+export const Default: Story<CheckboxProps> = (args) => (
+  <div style={{ display: 'flex', gap: 20, flexDirection: 'column' }}>
+    <Checkbox
+      name="category"
+      label="Action"
+      labelFor="action"
+      isChecked
+      {...args}
+    />
+    <Checkbox
+      name="category"
+      label="Adventure"
+      labelFor="adventure"
+      {...args}
+    />
+    <Checkbox name="category" label="Strategy" labelFor="strategy" {...args} />
+  </div>
+)

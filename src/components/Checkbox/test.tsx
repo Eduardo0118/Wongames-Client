693bca567650c6a85ef8dba71bb1b6a86dfcc4b5
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-12-08 20:50:01 -0300

    test: check if component's marked and verified focus

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,6 +1,6 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-
+import userEvent from '@testing-library/user-event'
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
@@ -26,5 +26,25 @@ describe('<Checkbox />', () => {
     expect(screen.getByText(/black label/i)).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} isChecked />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    renderWithTheme(<Checkbox label="Checkbox" />)
+
+    expect(document.body).toHaveFocus
+    userEvent.tab()
+    expect(screen.getByRole('checkbox')).toHaveFocus()
   })
 })

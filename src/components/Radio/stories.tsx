a938a0532cb021f398384aa03ed1b413ec76526c
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2022-01-04 12:50:35 -0300

    feat: radio component

--- src/components/Radio/stories.tsx
+++ src/components/Radio/stories.tsx
@@ -1,9 +1,48 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Radio from '.'
+import Radio, { RadioProps } from '.'
 
 export default {
   title: 'Radio',
-  component: Radio
+  component: Radio,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  argTypes: {
+    onCheck: { action: 'checked' }
+  }
 } as Meta
 
-export const Default: Story = () => <Radio />
+export const Default: Story<RadioProps> = (args) => (
+  <div
+    style={{ margin: 20, display: 'flex', flexDirection: 'column', gap: 20 }}
+  >
+    <Radio
+      label="primeiro"
+      labelFor="primeiro"
+      id="primeiro"
+      name="nome"
+      value="primeiro"
+      defaultChecked
+      {...args}
+    />
+    <Radio
+      label="segundo"
+      labelFor="segundo"
+      id="segundo"
+      name="nome"
+      value="segundo"
+      {...args}
+    />
+    <Radio
+      label="terceiro"
+      labelFor="terceiro"
+      id="terceiro"
+      name="nome"
+      value="terceiro"
+      {...args}
+    />
+  </div>
+)
